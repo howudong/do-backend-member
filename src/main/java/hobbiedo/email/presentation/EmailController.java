8562f7b910111138a91162c5ade959432d9ea40d
howudong <tjdvy963@naver.com>
2024-05-26 15:37:59 +0900

    refactor: 해당 브랜치 내용이 아닌데, 어쩌다가 여기까지 왔을까요? 제거했습니다.

--- src/main/java/hobbiedo/email/presentation/EmailController.java
+++ src/main/java/hobbiedo/email/presentation/EmailController.java
@@ -1,6 +1,5 @@
 package hobbiedo.email.presentation;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
