588fb22cc4fec2584f872bdcfc1b61b613d3cbf6
howudong <tjdvy963@naver.com>
2024-05-26 15:31:14 +0900

    refactor: 이메일 인증 코드 발송 관련 api가 VO와 DTO를 이용하도록 변경

--- src/main/java/hobbiedo/email/presentation/EmailController.java
+++ src/main/java/hobbiedo/email/presentation/EmailController.java
@@ -1,8 +1,10 @@
 package hobbiedo.email.presentation;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hobbiedo.email.application.EmailService;
