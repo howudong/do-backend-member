e5bdd97ad15403f152b84f47a274c5c58fe0584b
howudong <tjdvy963@naver.com>
2024-05-26 15:31:14 +0900

    refactor: 이메일 인증 코드 발송 관련 api가 VO와 DTO를 이용하도록 변경

--- src/main/java/hobbiedo/email/presentation/EmailController.java
+++ src/main/java/hobbiedo/email/presentation/EmailController.java
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hobbiedo.email.application.EmailService;
+import hobbiedo.email.converter.EmailAuthConverter;
 import hobbiedo.email.vo.request.EmailAuthVO;
+import hobbiedo.email.vo.request.EmailCheckVO;
 import hobbiedo.global.ApiResponse;
 import hobbiedo.global.code.status.SuccessStatus;
 import io.swagger.v3.oas.annotations.Operation;
@@ -26,7 +27,7 @@ public class EmailController {
 	@Operation(summary = "이메일 인증 코드 전송",
 		description = "이메일 인증 코드를 해당 이메일로 전송합니다.")
 	public ApiResponse<Void> sendEmailAuthApi(@RequestBody EmailAuthVO emailAuthVO) {
-		emailService.sendAuthCode(emailAuthVO.getEmail());
+		emailService.sendAuthCode(EmailAuthConverter.toDTO(emailAuthVO));
 		return ApiResponse.onSuccess(
 			SuccessStatus.SEND_AUTH_MAIL_SUCCESS,
 			null
--- src/main/java/hobbiedo/email/vo/request/EmailAuthVO.java
+++ src/main/java/hobbiedo/email/vo/request/EmailAuthVO.java
@@ -1,5 +1,6 @@
 package hobbiedo.email.vo.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "이메일 인증 코드 생성 요청 VO")
 public class EmailAuthVO {
+	@Schema(description = "이메일 인증 코드를 발송할 email")
 	private String email;
 }
