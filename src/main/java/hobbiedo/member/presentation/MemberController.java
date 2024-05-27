9a3f1519284f629264d37d02b711dfcc1bacc948
howudong <tjdvy963@naver.com>
2024-05-27 17:11:46 +0900

    style: api 이름 api 명세서에 맞춰 변경

--- src/main/java/hobbiedo/member/presentation/MemberController.java
+++ src/main/java/hobbiedo/member/presentation/MemberController.java
@@ -20,14 +20,15 @@ import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/non-users")
 @RequiredArgsConstructor
 @Tag(name = "Member", description = "사용자 정보 관련 API 입니다.")
 public class MemberController {
 	public static final String LOGIN_ID_PATTERN = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{8,20}$";
+
 	private final MemberService memberService;
 
-	@PostMapping("/sign-up/integration")
+	@PostMapping("/sign-up")
 	@Operation(summary = "통합 회원가입",
 		description = "통합 회원가입을 진행하여 새로운 회원을 등록합니다.")
 	public ApiResponse<Void> integrationSignUpApi(@RequestBody @Valid IntegrateSignUpVO signUpVO) {
@@ -38,10 +39,10 @@ public class MemberController {
 		);
 	}
 
-	@GetMapping("/sign-up/exists")
+	@GetMapping("/duplication")
 	@Operation(summary = "아이디 중복 확인",
 		description = "해당 아이디가 중복인지 확인합니다. 사용 가능하면 true, 그렇지 않으면 false를 반환합니다.")
-	public ApiResponse<ExistIdVO> existIdApi(
+	public ApiResponse<ExistIdVO> checkIdApi(
 		@RequestParam("loginId")
 		@Pattern(regexp = LOGIN_ID_PATTERN,
 			message = "아이디는 8~20자리의 영어+숫자로만 이뤄져야합니다.(특수 문자x)") String loginId) {
